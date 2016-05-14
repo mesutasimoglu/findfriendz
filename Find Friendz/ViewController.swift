0bbdcd05a95e9902e41a02c74b63c204fc309a18
mesutasimoglu <ASIMOGLU@mesut-macbook-pro.local>
2016-05-15 00:23:18 +0300

    Initial Commit

---
+++ Find Friendz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find Friendz
+//
+//  Created by Mesut Yılmaz on 15.05.2016.
+//  Copyright © 2016 Mesut Yılmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
